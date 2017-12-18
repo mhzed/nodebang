aad0af756ba96f2e32425da0461cff15ca484643
mhzed <minhongz@gmail.com>
2017-12-18 11:06:43 +0800

    change default indent size to 2

--- res/ts-react/app.tsx
+++ res/ts-react/app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 export interface AppProps { title: string; }
 export class App extends React.Component<AppProps, {}> {
-    render() {
-        return <h1>App {this.props.title}!</h1>;
-    }
+  render() {
+    return <h1>App {this.props.title}!</h1>;
+  }
 }
--- res/ts-react/index.tsx
+++ res/ts-react/index.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app";
 ReactDOM.render(
-    <App title="Nodebang app" />,
-    document.getElementById("main")
+  <App title="Nodebang app" />,
+  document.getElementById("main")
 );
